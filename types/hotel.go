97506b873a6dddce4f56fa2db460cd2d63b4817b
Niv B <nivb52@gmail.com>
2023-09-18 22:47:30 +0300

    types/new: Hotel

---
+++ types/hotel.go
@@ -0,0 +1,30 @@
+package types
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Hotel struct {
+	ID       primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	Name     string               `bson:"name" json:"name"`
+	Location string               `bson:"location" json:"location"`
+	Rooms    []primitive.ObjectID `bson:"rooms" json:"rooms"`
+	CreateAt primitive.DateTime   `bson:"create_at" json:"createAt"`
+	UpdateAt primitive.Timestamp  `bson:"update_at" json:"updateAt"`
+}
+
+type RoomType int
+
+const (
+	_ RoomType = iota
+	SingleRoomType
+	DoubleRoomType
+	SeaSideRoomType
+	DeluxRoomType
+)
+
+type Room struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Type      RoomType           `bson:"type" json:"type"`
+	BasePrice int                `bson:"base_price" json:"basePrice"`
+	Price     int                `bson:"price" json:"price"`
+	HotelID   primitive.ObjectID `bson:"hotelID" json:"hotelID"` // hotel_id ?
+}
