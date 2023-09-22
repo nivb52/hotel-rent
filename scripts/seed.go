00d75b6f356a315d54b8ec2f2e3c67e6657a9f3f
Niv B <nivb52@gmail.com>
2023-09-22 11:05:17 +0300

    scripts/new: seed rooms

--- scripts/seed.go
+++ scripts/seed.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/nivb52/hotel-rent/db"
@@ -27,6 +28,7 @@ func main() {
 	}
 
 	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+	roomStrore := db.NewMongoRoomStore(client, db.DBNAME)
 
 	// Create 10 hotel instances
 	seedHotels := []SeedHotel{
@@ -50,10 +52,82 @@ func main() {
 			UpdateAt: primitive.NewDateTimeFromTime(time.Now()),
 		}
 
+		randRoomType, randInt := getRandomRoomType()
+		numberOfRooms := randomIntByMaxAndMin(10, 1)
+
+		rooms := []types.Room{
+			{
+				Type:      randRoomType,
+				BasePrice: getRandomPrice(randInt),
+				CreateAt:  primitive.NewDateTimeFromTime(time.Now()),
+			},
+			{
+				Type:      randRoomType,
+				BasePrice: getRandomPrice(randInt),
+				CreateAt:  primitive.NewDateTimeFromTime(time.Now()),
+			},
+		}
+		for i := 0; i < numberOfRooms; i++ {
+			rooms = append(rooms, types.Room{
+				Type:      randRoomType,
+				BasePrice: getRandomPrice(randInt),
+				CreateAt:  primitive.NewDateTimeFromTime(time.Now()),
+				UpdateAt:  primitive.NewDateTimeFromTime(time.Now()),
+			})
+		}
+
 		insertedHotel, err := hotelStore.InsertHotel(context.TODO(), &hotel)
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println("New Hotel: ", insertedHotel)
+
+		for _, room := range rooms {
+			room.HotelID = insertedHotel.ID
+			insertedRoom, err := roomStrore.InsertRoom(context.TODO(), &room)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("New Room: ", insertedRoom)
+		}
+
 	}
+}
+
+// Helpers
+func getRandomRoomType() (types.RoomType, int) {
+	randInt := rand.Intn(int(types.ClosedRoomType))
+	switch randInt {
+	case 1:
+		return types.SingleRoomType, randInt
+	case 2:
+		return types.DoubleRoomType, randInt
+	case 3:
+		return types.SeaSideRoomType, randInt
+	case 4:
+		return types.DeluxRoomType, randInt
+	default:
+		return types.SingleRoomType, randInt
+	}
+}
+
+func getRandomPrice(ranInt int) int {
+	switch ranInt {
+	case 1:
+		return randomIntByMaxAndMin(99, 50) //types.SingleRoomType
+	case 2:
+		return randomIntByMaxAndMin(150, 100) //types.DoubleRoomType
+	case 3:
+		return randomIntByMaxAndMin(200, 150) //types.SeaSideRoomType
+	case 4:
+		return randomIntByMaxAndMin(500, 200) //types.DeluxRoomType
+	default:
+		return randomIntByMaxAndMin(100, 50) //types.SingleRoomType
+	}
+}
+
+// function which return random integer between to max and min
+func randomIntByMaxAndMin(max int, min int) int {
+	return rand.Intn(max-min) + min
 }
