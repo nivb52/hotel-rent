09eff32af7c2abf75c180e9767a2e3c701e1329e
Niv B <nivb52@gmail.com>
2023-09-26 08:50:21 +0300

    api/feat: option to use Rooms & Rating as query params

--- api/hotel_handler.go
+++ api/hotel_handler.go
@@ -17,7 +17,17 @@ func NewHotelHandler(hs db.HotelStore, rs db.RoomStore) *HotelHandler {
 	}
 }
 
+type HotelQueryParams struct {
+	Rooms  bool
+	Rating int
+}
+
 func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
+	var qParams HotelQueryParams
+	err := c.QueryParser(qParams)
+	if err != nil {
+		return err
+	}
 
 	hotels, err := h.hotelStore.GetHotels(c.Context())
 	if err != nil {
