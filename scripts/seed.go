0d1d8dce66037124a47a483ff5f2308abffa9ea4
Niv B <nivb52@gmail.com>
2023-10-02 21:33:22 +0300

    scripts/ref: mocks for user & hotel

--- scripts/seed.go
+++ scripts/seed.go
@@ -20,30 +20,55 @@ type SeedHotel struct {
 	Rating   int8
 }
 
+type SeedUser struct {
+	FName string
+	LName string
+	Email string
+}
+
 var (
 	client     *mongo.Client
 	hotelStore *db.MongoHotelStore
 	roomStore  *db.MongoRoomStore
+	userStore  *db.MongoUserStore
 	ctx        = context.Background()
 )
 
-func seedHotels(numberOfHotels int) {
+func seedUser(seed *SeedUser) error {
+	user, err := types.NewUserFromParams(types.UserParamsForCreate{
+		Email:     seed.Email,
+		FirstName: seed.FName,
+		LastName:  seed.LName,
+		Password:  "supersecretpassword",
+	})
 
-	// 10 hotel options
-	seedHotels10 := []SeedHotel{
-		{"The Ritz-Carlton", "Los Angeles, California", 5},
-		{"Grand Hyatt", "New York City, New York", 4},
-		{"Marriott Marquis", "Atlanta, Georgia", 3},
-		{"Four Seasons Resort", "Maui, Hawaii", 5},
-		{"Hilton Garden Inn", "Chicago, Illinois", 3},
-		{"Fairmont Empress", "Victoria, British Columbia, Canada", 5},
-		{"The Venetian", "Las Vegas, Nevada", 4},
-		{"Burj Al Arab Jumeirah", "Dubai, United Arab Emirates", 5},
-		{"The Savoy", "London, United Kingdom", 3},
-		{"Hotel del Coronado", "San Diego, California", 4},
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	seedHotels := seedHotels10[0:numberOfHotels]
+	_, err = userStore.InsertUser(ctx, user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func seedUsers(numberOfUsers int) int {
+	seedUsers := mockUsers(numberOfUsers)
+	var errors []error = make([]error, len(*seedUsers))
+	for _, user := range *seedUsers {
+		err := seedUser(&user)
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	return len(errors)
+}
+
+func seedHotels(numberOfHotels int) {
+	seedHotels := mockHotels(numberOfHotels)
 
 	for _, seedHotel := range seedHotels {
 		hotel := types.Hotel{
@@ -120,12 +145,130 @@ func main() {
 
 	hotelStore = db.NewMongoHotelStore(client, db.DBNAME)
 	roomStore = db.NewMongoRoomStore(client, db.DBNAME, hotelStore)
+	userStore = db.NewMongoUserStore(client, db.DBNAME)
 
 	seedHotels(6)
+	seedUsers(7)
+
 }
 
 func init() {
 	fmt.Println(" Seeding the db")
+}
+
+/** ============= Data ============= */
+func mockUsers(numberOfUsers int) *[]SeedUser {
+
+	usersMock := []SeedUser{
+		{"John", "Smith", "john.smith@email.com"},
+		{"Sarah", "Johnson", "sarah.johnson@email.com"},
+		{"Michael", "Brown", "michael.brown@email.com"},
+		{"Emily", "Davis", "emily.davis@email.com"},
+		{"David", "Wilson", "david.wilson@email.com"},
+		{"Jennifer", "Lee", "jennifer.lee@email.com"},
+		{"James", "Anderson", "james.anderson@email.com"},
+		{"Jessica", "Martinez", "jessica.martinez@email.com"},
+		{"Robert", "Thompson", "robert.thompson@email.com"},
+		{"Laura", "Garcia", "laura.garcia@email.com"},
+		{"William", "Taylor", "william.taylor@email.com"},
+		{"Olivia", "White", "olivia.white@email.com"},
+		{"Benjamin", "Harris", "benjamin.harris@email.com"},
+		{"Emma", "Clark", "emma.clark@email.com"},
+		{"Christopher", "Hall", "christopher.hall@email.com"},
+		{"Sophia", "Turner", "sophia.turner@email.com"},
+		{"Matthew", "Moore", "matthew.moore@email.com"},
+		{"Ava", "Parker", "ava.parker@email.com"},
+		{"Joseph", "Evans", "joseph.evans@email.com"},
+		{"Grace", "Carter", "grace.carter@email.com"},
+		{"Samuel", "Hughes", "samuel.hughes@email.com"},
+		{"Lily", "Bennett", "lily.bennett@email.com"},
+		{"Daniel", "Hayes", "daniel.hayes@email.com"},
+		{"Mia", "Foster", "mia.foster@email.com"},
+		{"Andrew", "Rivera", "andrew.rivera@email.com"},
+		{"Harper", "Gray", "harper.gray@email.com"},
+		{"Ethan", "Russell", "ethan.russell@email.com"},
+		{"Chloe", "Coleman", "chloe.coleman@email.com"},
+		{"Christopher", "Powell", "christopher.powell@email.com"},
+		{"Isabella", "Mitchell", "isabella.mitchell@email.com"},
+		{"Alexander", "Perry", "alexander.perry@email.com"},
+		{"Madison", "Ward", "madison.ward@email.com"},
+		{"Samuel", "Nelson", "samuel.nelson@email.com"},
+		{"Elizabeth", "Ramirez", "elizabeth.ramirez@email.com"},
+		{"Nicholas", "Jenkins", "nicholas.jenkins@email.com"},
+		{"Charlotte", "Wood", "charlotte.wood@email.com"},
+		{"Ryan", "Hayes", "ryan.hayes@email.com"},
+		{"Avery", "Green", "avery.green@email.com"},
+		{"Joshua", "Cooper", "joshua.cooper@email.com"},
+		{"Sofia", "Ross", "sofia.ross@email.com"},
+	}
+
+	res := usersMock[0:numberOfUsers]
+	return &res
+}
+
+func mockHotels(numberOfHotels int) []SeedHotel {
+	hotelsMock := []SeedHotel{
+		{"The Ritz-Carlton", "Los Angeles, California", 5},
+		{"Grand Hyatt", "New York City, New York", 4},
+		{"Marriott Marquis", "Atlanta, Georgia", 3},
+		{"Four Seasons Resort", "Maui, Hawaii", 5},
+		{"Hilton Garden Inn", "Chicago, Illinois", 3},
+		{"Fairmont Empress", "Victoria, British Columbia, Canada", 5},
+		{"The Venetian", "Las Vegas, Nevada", 4},
+		{"Burj Al Arab Jumeirah", "Dubai, United Arab Emirates", 5},
+		{"The Savoy", "London, United Kingdom", 3},
+		{"Hotel del Coronado", "San Diego, California", 4},
+		{"The Ritz-Carlton", "Los Angeles, California", 5},
+		{"Marriott Downtown", "New York City, New York", 4},
+		{"Four Seasons Hotel", "Miami, Florida", 5},
+		{"Hilton Riverside", "New Orleans, Louisiana", 4},
+		{"Hyatt Regency", "Chicago, Illinois", 4},
+		{"Sheraton Grand", "Phoenix, Arizona", 4},
+		{"Waldorf Astoria", "Beverly Hills, California", 5},
+		{"The Westin", "Seattle, Washington", 4},
+		{"Fairmont", "San Francisco, California", 5},
+		{"InterContinental", "Atlanta, Georgia", 4},
+		{"JW Marriott", "Las Vegas, Nevada", 4},
+		{"The Peninsula", "Chicago, Illinois", 5},
+		{"Renaissance", "Austin, Texas", 4},
+		{"Mandarin Oriental", "Boston, Massachusetts", 5},
+		{"Omni", "Nashville, Tennessee", 2},
+		{"Ritz-Carlton", "Dallas, Texas", 5},
+		{"Hilton Garden Inn", "Orlando, Florida", 3},
+		{"Grand Hyatt", "San Diego, California", 4},
+		{"Kimpton", "Portland, Oregon", 4},
+		{"Radisson Blu", "Minneapolis, Minnesota", 4},
+		{"DoubleTree by Hilton", "Philadelphia, Pennsylvania", 4},
+		{"Hilton", "Denver, Colorado", 3},
+		{"Aloft", "Raleigh, North Carolina", 3},
+		{"The Langham", "New York City, New York", 1},
+		{"Embassy Suites", "San Antonio, Texas", 3},
+		{"Ritz-Carlton", "Naples, Florida", 5},
+		{"Hilton", "Indianapolis, Indiana", 4},
+		{"The Waldorf Astoria", "Las Vegas, Nevada", 5},
+		{"The Omni", "Louisville, Kentucky", 4},
+		{"The St. Regis", "Washington, D.C.", 5},
+		{"Fairfield Inn & Suites", "Houston, Texas", 3},
+		{"Hilton", "Tampa, Florida", 4},
+		{"Hyatt", "San Jose, California", 4},
+		{"Marriott", "St. Louis, Missouri", 1},
+		{"Sheraton", "Salt Lake City, Utah", 4},
+		{"The Ritz-Carlton", "Maui, Hawaii", 5},
+		{"InterContinental", "Miami, Florida", 4},
+		{"JW Marriott", "Orlando, Florida", 4},
+		{"Hilton Garden Inn", "Louisville, Kentucky", 3},
+		{"Four Seasons", "New York City, New York", 5},
+		{"The Westin", "Denver, Colorado", 4},
+		{"The Peninsula", "Beverly Hills, California", 5},
+		{"Waldorf Astoria", "Orlando, Florida", 5},
+		{"Renaissance", "Las Vegas, Nevada", 2},
+		{"Hyatt Regency", "Seattle, Washington", 4},
+		{"DoubleTree by Hilton", "San Diego, California", 4},
+		{"Kimpton", "Chicago, Illinois", 4},
+	}
+
+	res := hotelsMock[0:numberOfHotels]
+	return res
 }
 
 /** ============= Helpers ============= */
