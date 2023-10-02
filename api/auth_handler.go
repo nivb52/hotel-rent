187e8421c34ebb29835a137f1ce662c5ea9822b0
Niv B <nivb52@gmail.com>
2023-10-02 22:07:50 +0300

    api/feat: auth route - compare passwords

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/nivb52/hotel-rent/db"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -39,7 +40,12 @@ func (h *AuthHandler) HandleAuth(c *fiber.Ctx) error {
 		return err
 	}
 
-	fmt.Println("found user by email", user)
+	err = bcrypt.CompareHashAndPassword([]byte(user.EncryptedPassword), []byte(params.Password))
+	if err != nil {
+		fmt.Println("Failed Compare Hash and Password")
+		return fmt.Errorf("invalid credentials")
+	}
 
+	fmt.Println("Authenticated user ", user)
 	return nil
 }
