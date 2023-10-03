549edaf3ac55eebdd3ab7c603ec995e780cd0aae
Niv B <nivb52@gmail.com>
2023-10-03 22:46:02 +0300

    api/feat: complete Auth Handler

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -3,11 +3,14 @@ package api
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/nivb52/hotel-rent/api/middleware"
 	"github.com/nivb52/hotel-rent/db"
+	"github.com/nivb52/hotel-rent/types"
 	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -25,6 +28,11 @@ type AuthParams struct {
 	Password string `json:"password"`
 }
 
+type AuthResponse struct {
+	User  *types.User `json:"user,omitempty"`
+	Token string      `json:"token,omitempty"`
+}
+
 func (h *AuthHandler) HandleAuth(c *fiber.Ctx) error {
 	var params AuthParams
 	err := c.BodyParser(&params)
@@ -40,12 +48,39 @@ func (h *AuthHandler) HandleAuth(c *fiber.Ctx) error {
 		return err
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.EncryptedPassword), []byte(params.Password))
-	if err != nil {
-		fmt.Println("Failed Compare Hash and Password")
+	isValid := types.IsPasswordValid(user.EncryptedPassword, params.Password)
+	if !isValid {
 		return fmt.Errorf("invalid credentials")
 	}
 
-	fmt.Println("Authenticated user ", user)
-	return nil
+	tokenString, err := createToken(user.ID.Hex(), user.Email)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Authenticated user")
+	resp := AuthResponse{
+		User:  user,
+		Token: tokenString,
+	}
+	return c.JSON(resp)
+}
+
+// function to create token From User data - id and email.
+func createToken(userID, userEmail string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"id":    userID,
+			"email": userEmail,
+			"exp":   time.Now().Add(time.Hour * 4).Unix(),
+		})
+
+	secret := middleware.GetSecret()
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		fmt.Println("Failed to sign token with secret, userID: ", userID, " useEmail: ", userEmail)
+		return "", err
+	}
+
+	return tokenString, nil
 }
