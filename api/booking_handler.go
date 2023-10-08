6dc8441aa9cf8acae3d82d92625534c695acbdba
Niv B <nivb52@gmail.com>
2023-10-08 22:30:40 +0300

    api/feat: get bookings list

--- api/booking_handler.go
+++ api/booking_handler.go
@@ -20,6 +20,40 @@ func NewBookingHandler(store *db.Store) *BookingHandler {
 	}
 }
 
+// --- GET Bookings ---
+func (h *BookingHandler) GetBookings(c *fiber.Ctx) error {
+	var (
+		roomID      = c.Params("id")
+		whereClause types.BookingFilter
+	)
+
+	if roomID == "" {
+		fmt.Printf("\n Booking Data missing RoomID: %s", roomID)
+		return c.Status(fiber.ErrBadRequest.Code).SendString(fiber.ErrBadRequest.Message)
+	}
+
+	err := c.BodyParser(&whereClause)
+	if err != nil {
+		fmt.Println("Booking Params to create - Failed to parse, due: ", err)
+		return c.Status(fiber.ErrBadRequest.Code).SendString(fiber.ErrBadRequest.Message)
+	}
+
+	fmt.Println("whereClause: ", whereClause)
+	whereClause.RoomID = roomID
+	bookings, err := h.store.Booking.GetBookings(c.Context(), &whereClause)
+	if err != nil {
+		fmt.Println("GetBookings with filter failed, due: ", err)
+		return c.Status(fiber.StatusInternalServerError).SendString(fiber.ErrInternalServerError.Message)
+	}
+
+	fmt.Println("bookings: ", bookings)
+	if len(bookings) > 0 {
+		return c.JSON(bookings)
+	}
+
+	return c.Status(fiber.StatusNotFound).SendString(fiber.ErrNotFound.Message)
+}
+
 //	--- Make Bookings ---
 //
 // Make a booking without register
