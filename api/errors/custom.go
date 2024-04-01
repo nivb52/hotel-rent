047afc2433e3b46e4fdf152b0073c1234105e98f
Niv B <nivb52@gmail.com>
2024-04-02 01:18:01 +0300

    api/new: custom errors

---
+++ api/errors/custom.go
@@ -0,0 +1,87 @@
+package customerrors
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type ServerError struct {
+	Code   int    `json:"code"`
+	Msg    string `json:"msg"`
+	Reason string `json:"reason"`
+}
+
+// impl. error iterface
+func (e ServerError) Error() string {
+	return e.Msg
+}
+
+func NewServerError(code int, msg string) ServerError {
+	reason := ""
+	return ServerError{
+		Code:   code,
+		Msg:    msg,
+		Reason: reason,
+	}
+}
+
+func ErrUnAuthorized() ServerError {
+	return ServerError{
+		Code:   http.StatusUnauthorized,
+		Msg:    "Unauthorized",
+		Reason: "",
+	}
+}
+
+func ErrTokenExpired() ServerError {
+	return ServerError{
+		Code:   http.StatusUnauthorized,
+		Msg:    "token expired",
+		Reason: "",
+	}
+}
+
+func ErrInvalidID(reason string) ServerError {
+	return ServerError{
+		Code:   http.StatusBadRequest,
+		Msg:    "the provided ID is wrong",
+		Reason: reason,
+	}
+}
+
+func ErrForbidden(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusForbidden).SendString(fiber.ErrForbidden.Message)
+}
+
+func ErrBadRequest(c *fiber.Ctx, reason ...string) error {
+	msg := fiber.ErrBadRequest.Message
+	if len(reason) > 0 {
+		msg = reason[0]
+	}
+	return c.Status(fiber.ErrBadRequest.Code).SendString(msg)
+}
+
+func ErrResourceNotFound(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusNotFound).SendString(fiber.ErrNotFound.Message)
+}
+
+func ErrMethodNotAllowed(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusMethodNotAllowed).SendString(fiber.ErrMethodNotAllowed.Message)
+}
+
+func ErrConflict(c *fiber.Ctx, reason ...string) error {
+	msg := fiber.ErrConflict.Message
+	if len(reason) > 0 {
+		msg = reason[0]
+	}
+	return c.Status(fiber.ErrConflict.Code).SendString(msg)
+}
+
+func ErrInternalServerError(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusInternalServerError).SendString(fiber.ErrInternalServerError.Message)
+}
+
+func ErrNotImplemented(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusNotImplemented).SendString(fiber.ErrNotImplemented.Message)
+}
