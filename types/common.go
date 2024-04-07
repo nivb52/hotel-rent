3d45c078cd06b8c15fcd36a4ce9d8d01354e3005
Niv B <nivb52@gmail.com>
2024-04-07 09:22:21 +0300

    types/feat: move common types, DatesFilter + map any

--- types/common.go
+++ types/common.go
@@ -1,3 +1,10 @@
 package types
 
+import "time"
+
 type Map map[string]any
+
+type DatesFilter struct {
+	FromDate time.Time `bson:"fromDate,omitempty" json:"fromDate,omitempty"`
+	TillDate time.Time `bson:"tillDate,omitempty" json:"tillDate,omitempty"`
+}
